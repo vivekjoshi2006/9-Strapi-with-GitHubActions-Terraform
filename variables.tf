bf7875644202853e6d3648dafb0846037bdeecd0
Vivek Joshi <vivekjoshi41996@gmail.com>
2026-02-16 09:52:20 +0000

    Final Fix: Corrected paths and added all missing variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "key_name" {
   description = "The name of the EC2 key pair"
   type        = string
