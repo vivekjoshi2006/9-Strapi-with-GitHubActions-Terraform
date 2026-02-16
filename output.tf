c583e849bb479b1cf80091a902d8c9a75f5496de
Vivek Joshi <vivekjoshi41996@gmail.com>
2026-02-16 10:26:53 +0000

    Fix: Removed duplicate output.tf file

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  description = "The Public IP of your Strapi Server"
-  value       = aws_instance.strapi_server.public_ip
-}
-
-output "strapi_admin_url" {
-  description = "Click this link to verify your deployment"
-  value       = "http://${aws_instance.strapi_server.public_ip}:1337/admin"
-}
