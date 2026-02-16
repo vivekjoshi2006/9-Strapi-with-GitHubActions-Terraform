b8813728c8e28a19d6f78da32a1ad728bf4e7d6c
Vivek Joshi <vivekjoshi41996@gmail.com>
2026-02-16 10:22:46 +0000

    Final main.tf with SG v6 and no key_name

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi_sg_final_v4" # Phir se naya naam taaki error na aaye
+  name        = "strapi_sg_final_v6" # Unique name to avoid duplicate error
   description = "Allow inbound traffic for Strapi"
 
+  # SSH Access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -26,10 +27,10 @@ resource "aws_security_group" "strapi_sg" {
 
 resource "aws_instance" "strapi_server" {
   ami           = "ami-0c7217cdde317cfec" 
-  instance_type = "t2.micro" # Seedha yahan likh diya
+  instance_type = "t2.micro"              
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
-  # key_name line poori tarah hata di gayi hai
 
+  # User Data to install Docker and run Strapi
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update -y
@@ -45,6 +46,14 @@ resource "aws_instance" "strapi_server" {
   }
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.strapi_server.public_ip
+}
+
 output "strapi_url" {
   value = "http://${aws_instance.strapi_server.public_ip}:1337"
+}
+
+output "strapi_admin_url" {
+  value = "http://${aws_instance.strapi_server.public_ip}:1337/admin"
 }
